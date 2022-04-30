29fe94c68fca51d15fce2d550165916cf19a70ae
Victor Emmanuel Lawani <37407454+Lawaniv12@users.noreply.github.com>
2022-04-30 15:04:48 +0100

    first commit

---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,45 @@
+export const navbarData = [
+  {
+    routeLink: 'dashboard',
+    icon: "fa-solid fa-house-user",
+    label: 'Dashboard'
+  },
+
+  {
+    routeLink: 'products',
+    icon: "fa-solid fa-box",
+    label: 'Products'
+  },
+
+  {
+    routeLink: 'statistics',
+    icon: "fa-solid fa-chart-bar",
+    label: 'Statistics'
+  },
+
+  {
+    routeLink: 'coupens',
+    icon: "fa-solid fa-tags",
+    label: 'Coupens'
+  },
+
+  {
+    routeLink: 'pages',
+    icon: "fa-solid fa-file",
+    label: 'Pages'
+  },
+
+  {
+    routeLink: 'media',
+    icon: "fa-solid fa-camera",
+    label: 'Media'
+  },
+
+  {
+    routeLink: 'settings',
+    icon: "fa-solid fa-gear",
+    label: 'Settings'
+  }
+
+
+]
